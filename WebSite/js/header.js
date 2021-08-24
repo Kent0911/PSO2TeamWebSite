a6585bc0bbd5a80a064f3a3c596f8bb46f2afd3f
Kent0911 <mots_de_la_magie.s@outlook.jp>
2021-08-25 01:19:40 +0900

    menu.js renamed

--- WebSite/js/header.js
+++ WebSite/js/header.js
@@ -1,13 +1,13 @@
 const parent = document.getElementsByClassName("ship1_teams");
 
-var node = Array.prototype.slice.call(parent, 0);
+let node = Array.prototype.slice.call(parent, 0);
 
 node.forEach(element => {
-    element.addEventListener("mouseover", function() {
+    element.addEventListener("mouseover", () => {
         element.querySelector(".ship1_teams_dropdown").classList.add("active");
     }, false);
 
-    element.addEventListener("mouseout", function() {
+    element.addEventListener("mouseout", () => {
         element.querySelector(".ship1_teams_dropdown").classList.remove("active");
     }, false);
 });
